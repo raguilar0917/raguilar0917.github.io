8d6056f2b4d1cd2303aa979bc979857c40bcf427
Richard Aguilar <44679819+raguilar0917@users.noreply.github.com>
2020-03-02 18:41:10 -0800

    Update jquery.particleground.js

--- js/jquery.particleground.js
+++ js/jquery.particleground.js
@@ -1,8 +1,6 @@
 /*!
  * Particleground
  *
- * @author Jonathan Nicol - @mrjnicol
- * @version 1.1.0
  * @description Creates a canvas based particle system background
  *
  * Inspired by http://requestlab.fr/ and http://disruptivebydesign.com/
