cd64402540be210d685b912e5611359d1042eb1c
Richard Aguilar <44679819+raguilar0917@users.noreply.github.com>
2020-09-02 23:28:48 -0700

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -1,9 +1,4 @@
-/**
- * Particleground demo
- * @author Jonathan Nicol - @mrjnicol
- */
 
-// This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
     dotColor: '#5cbdaa',
